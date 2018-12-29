d72dedd89e2987a3a85cf855ce3dfd05c1477ab1
Юханов Сергей Сергеевич <s.yukhanov@gmail.com>
2018-12-29 14:54:31 +0300

    Added recognize functionality

--- SpeachNote/ViewController.swift
+++ SpeachNote/ViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 import Speech
 
-class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var textField: UITextField!
+    
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
     
     let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ru"))
     var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-    var regognitionTask: SFSpeechRecognitionTask?
+    var recognitionTask: SFSpeechRecognitionTask?
     let audioEngine = AVAudioEngine()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        textView.textColor = UIColor.gray
         recordButton.isEnabled = false
         speechRecognizer?.delegate = self
         
@@ -46,9 +48,83 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             }
         }
     }
+    func startRecording() {
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do { // 3
+            try audioSession.setCategory(AVAudioSession.Category.record, mode: AVAudioSession.Mode.measurement, policy: AVAudioSession.RouteSharingPolicy.default, options: AVAudioSession.CategoryOptions.mixWithOthers)
+            try audioSession.setActive(true)
+        } catch {
+            print("Не удалось настроить аудиосессию")
+        }
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Не могу создать экземпляр запроса")
+        }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            var isFinal = false
+            if result != nil {
+                self.textView.textColor = .black
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = ((result?.isFinal)!)
+            }
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.recordButton.isEnabled = true
+            }
+        })
+        let format = inputNode.outputFormat(forBus: 0) // 12
+        
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: format) { // 13
+            buffer, _ in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare() //14
+        
+        do {  // 15
+            try audioEngine.start()
+        } catch {
+            print("Не удается стартнуть движок")
+        }
+        
+        textView.text = "Помедленнее... Я записываю...." // 16
+  
+    }
 
     @IBAction func recordButtonTapped(_ sender: UIButton) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            recordButton.isEnabled = false
+            recordButton.setTitle("Начать запись", for: .normal)
+        } else {
+            startRecording()
+            recordButton.setTitle("Остановить запись", for: .normal)
+        }
     }
     
 }
 
+extension ViewController: SFSpeechRecognizerDelegate {
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            recordButton.isEnabled = true
+        } else {
+            recordButton.isEnabled = false
+        }
+    }
+}
